c0237b8969eb907b6a850a0d3536960e55f25ec8
Nguyễn Thái Son Ny <134358623+nguyenthaisonny@users.noreply.github.com>
2024-11-01 19:44:42 +0700

    Clerk AUth

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const NavBar = () => {
   return (
@@ -20,6 +21,9 @@ const NavBar = () => {
         <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Noom</p>
       </Link>
       <div className='flex-between gap-5'>
+          <SignedIn>
+            <UserButton />
+            </SignedIn>
         <MobileNav/>
       </div>
     </nav>
