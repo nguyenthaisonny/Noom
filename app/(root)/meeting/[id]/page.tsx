c749b0bc446b1faa255982a92d0331f18e8cb7ed
Nguyễn Thái Son Ny <134358623+nguyenthaisonny@users.noreply.github.com>
2024-11-11 01:29:05 +1100

    type safe meeting page

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -3,27 +3,40 @@ import Loader from '@/components/Loader';
 import MeetingRoom from '@/components/MeetingRoom';
 import MeetingSetup from '@/components/MeetingSetup';
 import { useGetCallById } from '@/hooks/useGetCallById';
-import { useUser } from '@clerk/nextjs'
+import { useUser } from '@clerk/nextjs';
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-const Meeting = ({ params: {id} } : { params: { id: string } }) => {
-  const { isLoaded } = useUser();
-  const [isSetupComplete, setIsSetupComplete] = useState(false)
-  const {call, isCallLoading} = useGetCallById(id)
-  if(isCallLoading || !isLoaded) return <Loader/>
-
-  return (
-    <main className='h-screen w-full'>
-      <StreamCall call={call}>
-        <StreamTheme>
-          {!isSetupComplete ? (
-            <MeetingSetup setIsSetupComplete={setIsSetupComplete}/>
-          ) : ( <MeetingRoom/> )}
-        </StreamTheme>
-      </StreamCall>
-    </main>
-  )
+interface MeetingProps {
+  params: {
+    id: string;
+  };
 }
 
-export default Meeting
+const Meeting: React.FC<MeetingProps> = ({ params: { id } }) => {
+  const { isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (isCallLoading || !isLoaded) return <Loader />;
+
+  return (
+    <main className="h-screen w-full">
+      {call ? (
+        <StreamCall call={call}>
+          <StreamTheme>
+            {!isSetupComplete ? (
+              <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+            ) : (
+              <MeetingRoom />
+            )}
+          </StreamTheme>
+        </StreamCall>
+      ) : (
+        <Loader /> // Or handle missing call differently
+      )}
+    </main>
+  );
+};
+
+export default Meeting;
